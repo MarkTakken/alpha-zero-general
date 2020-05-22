5b530a31edcaeaf39d8c4b9a3e0ce08fd544fe90
Mark <marik@ToddsMacMini.local>
2020-05-22 13:12:16 -0400

    Implemented go_nn

--- go/go_nn.py
+++ go/go_nn.py
@@ -5,19 +5,30 @@ import numpy as np
 import torch
 from torch import Tensor, nn
 
+class ConvBlock(nn.Module):
+    def __init__(self,
+                 in_channels: int=17,
+                 out_channels: int=256):
+        self.extractor = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1), #changed outchannels = 256 to outchannels = outchannels
+            nn.BatchNorm2d(num_features=out_channels),  #Mark 5/22: Changed 256 to out_channels
+            nn.ReLU(inplace=True),
+        )
+    
+    def forward(self, x: Tensor) -> Tensor:
+        return self.extractor(x)
 
 class ResidualBlock(nn.Module):
     def __init__(self,
                  in_channels: int=256, #Mark 5/21: Should default to 256 (instead of 17) because it receives as input the output of the convolutional block?
+                 inter_channels: int=256,
                  out_channels: int=256):
-        self.in_channels = in_channels
-        self.out_channels = out_channels
         self.extractor = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(num_features=256),
+            nn.Conv2d(in_channels=in_channels, out_channels=inter_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=inter_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(num_features=256),
+            nn.Conv2d(in_channels=inter_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=out_channels),
         )
         self.relu = nn.ReLU(inplace=True)
     
@@ -26,33 +37,52 @@ class ResidualBlock(nn.Module):
         x_out = x + x_features
         return self.relu(x_out) #Mark 5/21: Changed x to x_out
 
+class Flatten(nn.Module):
+    def forward(self,x):
+        return x.view(-1)
 
-class ConvBlock(nn.Module):
+
+class PolicyHead(nn.Module):
     def __init__(self,
-                 in_channels: int=17,
-                 out_channels: int=256):
-        self.in_channels = in_channels
-        self.out_channels = out_channels
+                 in_channels : int = 256,
+                 board_size: int = 19,
+                 conv_out_channels: int = 2):
         self.extractor = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(num_features=256),
-            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels = in_channels, out_channels = conv_out_channels, kernel_size=1,stride=1,padding=0),
+            nn.BatchNorm2d(num_features = conv_out_channels),
+            nn.ReLU(inplace = True),
+            Flatten(),
+            nn.Linear(board_size**2*conv_out_channels,board_size**2+1)
         )
-    
-    def forward(self, x: Tensor) -> Tensor:
+
+    def forward(self,x):
         return self.extractor(x)
 
+class ValueHead(nn.Module):
+    def __init__(self,
+                 in_channels : int = 256,
+                 board_size: int = 19,
+                 conv_out_channels: int = 1,
+                 hidden_layer_size: int = 256):
+        self.extractor = nn.Sequential(
+            nn.Conv2d(in_channels = in_channels, out_channels = conv_out_channels, kernel_size = 1, stride = 1, padding = 0),
+            nn.BatchNorm2d(num_features = conv_out_channels),
+            nn.ReLU(inplace = True),
+            Flatten(),
+            nn.Linear(board_size**2*conv_out_channels,hidden_layer_size),
+            nn.Linear(hidden_layer_size,1)
+        )
+
+    def forward(self,x):
+        return self.extractor(x)
 
 class GoCNN(nn.Module):
     def __init__(self,
                  board_size: int=19, #Mark 5/21: Changed 9 to 19
-                 history_size_per_player: int=8,
+                 history: int=8,
                  n_blocks: int=3,  # Alphago Zero uses 19 or 39 blocks, we use less. 
                  n_filters: int=256):
-        self.board_size = board_size
-        self.history_size_per_player = history_size_per_player
-        self.in_channels = history_size_per_player * 2 + 1
-        self.input_shape = [board_size, board_size, self.in_channels]
+        self.in_channels = history * 2 + 1
         
         self.conv_block = ConvBlock(in_channels=self.in_channels, out_channels=256)
         
@@ -60,19 +90,21 @@ class GoCNN(nn.Module):
         for i in range(n_blocks):
             resblock = ResidualBlock(in_channels=n_filters, out_channels=n_filters)
             self.blocks.append(resblock)
-    
-    def forward(self, x: Tensor) -> Tensor:
+        self.policy_head = PolicyHead(in_channels = n_filters, board_size = board_size)
+        self.value_head = ValueHead(in_channels = 256, board_size = board_size)
+
+    def forward(self, x) -> Tensor:
+        x = Tensor(x)
         x = self.conv_block(x)
         for block in self.blocks:
             x = block(x)
-        return x
+        pi = self.policy_head(x)
+        v = self.value_head(x)
+        return (pi,v)
 
-
-class PolicyHead(nn.Module):
-    def __init__(self,
-                 in_channels = 256,
-                 out_channels = 256):
-        self.in_channels = 256 #Not finished yet
-
-class ValueHead(nn.Module):
-    raise NotImplementedError("TODO:")
+def test():
+    network = GoCNN(board_size=4,history=1)
+    canonical_state = np.array([[[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]],[[0,1,0,0],[1,1,0,0],[0,0,0,1],[1,0,0,0]],[[0,0,0,0],[0,0,1,1],[1,0,0,0],[0,1,1,0]]])
+    print(canonical_state)
+    print('------------')
+    print(network(canonical_state))
