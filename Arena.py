dd937d89b666db6ff709e1c5b61cbd179a3eb45e
Mark Takken <marktakken@Marks-MacBook-Air.local>
2020-09-15 14:01:36 -0400

    Changed parameters, added print statement 9/15

--- Arena.py
+++ Arena.py
@@ -38,6 +38,7 @@ class Arena():
         board = self.game.getInitBoard()
         it = 0
         while self.game.getGameEnded(board, curPlayer)==0:
+            print(it)
             it+=1
             if verbose:
                 assert(self.display)
