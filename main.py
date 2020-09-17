13d15f0d8d939851ed444070c8931e98f2060ee3
Mark Takken <marktakken@Marks-MacBook-Air.local>
2020-09-17 12:46:31 -0400

    9/17

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ args = dotdict({
     'updateThreshold': 0.5,     #Originally 0.6 # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'maxlenOfQueue': 200000,    #Originally 200000 # Number of game examples to train the neural networks.
     'numMCTSSims': 40,          #Originally 25 # Number of games moves for MCTS to simulate.
-    'arenaCompare': 20,         #Originally 40 # Number of games to play during arena play to determine if new net will be accepted.
+    'arenaCompare': 10,         #Originally 40 # Number of games to play during arena play to determine if new net will be accepted.
     'cpuct': 1,
 
     'checkpoint': './trained_models/9x9go',
