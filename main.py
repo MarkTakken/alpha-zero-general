5619a01d39dbfd5d8f58f15a7a8a4a3cac5eb7ef
Mark Takken <marktakken@Marks-MacBook-Air.local>
2020-09-18 15:55:55 -0400

    9/18

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if "-d" in argv or "--debug" in argv:
     root_logger.setLevel(logging.DEBUG)
 
 args = dotdict({
-    'startIter': 3,
+    'startIter': 4,
     'numIters': 1000,  #Originally 1000
     'numEps': 120,        #Originally 100    # Number of complete self-play games to simulate during a new iteration.
     'tempThreshold': 9,        #Originally 15
@@ -34,7 +34,7 @@ args = dotdict({
     'load_model': True,
     'load_folder_file': ('./trained_models/9x9go','checkpoint_1.pth.tar'), #Originally /dev/models/8x100x50 , best.pth.tar
     'load_folder_file_pnet': None, #('./trained_models/9x9go','checkpoint_0.pth.tar'),
-    'load_folder_file_examples': ('./trained_models/9x9go','checkpoint_2.pth.tar.examples'), #added
+    'load_folder_file_examples': ('./trained_models/9x9go','checkpoint_3.pth.tar.examples'), #added
     'skipFirstSelfPlay': False,
     'skipFirstTrain': False,
     'numItersForTrainExamplesHistory': 20, #Originally 20
